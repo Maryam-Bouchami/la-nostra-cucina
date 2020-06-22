993fe84023c7e4c629aed17b96ca6dc1116d6145
Maryam Bouchami <maryambouchami@gmail.com>
2020-06-22 11:43:50 +0200

    Premier commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+
+
+if (document.body.clientWidth > 991.98){
+	
+	
+window.addEventListener("scroll", function () {
+
+		let menu = document.querySelector(".row-menu , #inter-menu");
+		if (window.pageYOffset > 370) {
+			menu.style.backgroundColor = "rgb(255, 255, 255)";
+		}
+        else{
+			menu.style.backgroundColor = "transparent";
+		}
+       
+
+	}
+
+
+);
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+	
